5659a2191110a68c8fec1990f607c4a4d5a8594d
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-01-23 10:39:32 -0800

    Fix task execution policy concat

--- infra/tofu_importable_modules/bloom_deployment/ecs.tf
+++ infra/tofu_importable_modules/bloom_deployment/ecs.tf
@@ -82,7 +82,7 @@ resource "aws_secretsmanager_secret" "api_jwt_signing_key" {
 locals {
   roles = {
     "api" = {
-      task_execution_policy_extra_statements = [
+      task_execution_policy_extra_statements = concat([
         {
           Action   = "secretsmanager:GetSecretValue"
           Effect   = "Allow"
@@ -93,14 +93,14 @@ locals {
           Effect   = "Allow"
           Resource = aws_secretsmanager_secret.api_jwt_signing_key.arn
         },
+        ],
         var.bloom_api_fast_api_key_secret_arn != "" ? [
           {
             Action   = "secretsmanager:GetSecretValue"
             Effect   = "Allow"
             Resource = var.bloom_api_fast_api_key_secret_arn
           }
-        ] : []
-      ]
+      ] : [])
     }
     "site-partners" = {
       task_execution_policy_extra_statements = []
